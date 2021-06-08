d8354ff6aac2be248a60a94832ceaf651e909f0b
mLunday <54635976+mLunday@users.noreply.github.com>
2021-06-08 09:25:10 -0500

    Added my info

--- hello.c
+++ hello.c
@@ -6,6 +6,9 @@ int main(int argc, char **argv) {
 
   printf("Corbin Lamplot\n");
   printf("Computer Science\n");
+  
+  printf("\nMara Lunday\n");
+  printf("Mechanical Engineer");
 
   return 0;
 }
